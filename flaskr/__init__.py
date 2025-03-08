1a448062c8ae603fd1f5d43f2c95963d2f8e5ec0
Teddy Malhan <teddymalhan@d192-075-241-200.wireless.sfu.ca>
2025-03-08 11:21:17 -0800

    added pseudocode

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,5 +3,15 @@ from .views import bp as main_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main_bp)
+
+    # Get the text prompt from the frontend
+    # 
+    # Get a list of keywords from OpenAI API
+    # 
+    # Pass that list of keywords to the YouTube API
+    # 
+    # Make a random list of videos with those keywords, plus a list with an attribute that its a fun video
+    # 
+    # ['Video1', 'Video2', 'Cat Video', Video1] 
+
     return app
